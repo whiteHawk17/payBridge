a31a4a98a8ebd2acb8c86c3386439acdf786936b
whiteHawk7 <vini17102005@gmail.com>
2025-07-22 00:10:54 +0530

    fix some endpoints

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../config/env');
 
-const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRES_IN = '7d'; // You can adjust as needed
 
 function signToken(user) {
+  
   return jwt.sign(
     {
       id: user._id,
@@ -18,7 +19,9 @@ function signToken(user) {
 }
 
 function verifyToken(token) {
+
   return jwt.verify(token, JWT_SECRET);
+
 }
 
 module.exports = { signToken, verifyToken }; 
